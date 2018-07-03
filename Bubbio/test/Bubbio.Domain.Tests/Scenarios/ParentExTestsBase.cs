ed337921fda02456d30e40b9e71c1a973a8f88e9
Martin Ponce <martin.ponce@outlook.com>
2018-07-03 18:59:08 +0800

    Validate parent name and id

---
+++ Bubbio/test/Bubbio.Domain.Tests/Scenarios/ParentExTestsBase.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Bubbio.Core;
+using Bubbio.Core.Exceptions;
+using Bubbio.Domain.Validators;
+using Bubbio.Tests.Core.Builders;
+using Xunit;
+
+namespace Bubbio.Domain.Tests.Scenarios
+{
+    public class ParentExTestsBase
+    {
+        private Parent _parentPreValidation;
+        private Parent _parentPostValidation;
+
+        protected string NamePreValidation;
+        protected string NamePostValidation;
+
+        protected void ParentIsValidated()
+        {
+            try
+            {
+                _parentPostValidation = _parentPreValidation.Validate();
+            }
+            catch (InvalidNameException) {}
+            catch (InvalidIdException) {}
+        }
+
+        protected void ParentWithFirstName(string name) =>
+            _parentPreValidation = new ParentBuilder()
+                .WithFirstName(name)
+                .Build();
+
+        protected void FirstNameIsFormatted(string expected) =>
+            Assert.Equal(expected, _parentPostValidation.Name.First);
+
+        protected void ParentWithMiddleName(string name) =>
+            _parentPreValidation = new ParentBuilder()
+                .WithMiddleName(name)
+                .Build();
+
+        protected void MiddleNameIsFormatted(string expected) =>
+            Assert.Equal(expected, _parentPostValidation.Name.Middle);
+
+        protected void ParentWithLastName(string name) =>
+            _parentPreValidation = new ParentBuilder()
+                .WithLastName(name)
+                .Build();
+
+        protected void LastNameIsFormatted(string expected) =>
+            Assert.Equal(expected, _parentPostValidation.Name.Last);
+
+        protected void ParentWithoutId() =>
+            _parentPreValidation = new ParentBuilder()
+                .WithId(new Guid())
+                .Build();
+
+        protected void ParentIsInvalid() =>
+            Assert.Null(_parentPostValidation);
+    }
+}
---
+++ Bubbio/test/Bubbio.Tests.Core/Builders/ParentBuilder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Bubbio.Core;
+
+namespace Bubbio.Tests.Core.Builders
+{
+    public sealed class ParentBuilder
+    {
+        private readonly Parent _parent;
+
+        public ParentBuilder()
+        {
+            _parent = new Parent
+            {
+                Name = new Name { First = "Kim", Middle = "Chi", Last = "Ponce" }
+            };
+        }
+
+        public ParentBuilder WithId(Guid id)
+        {
+            _parent.Id = id;
+            return this;
+        }
+
+        public ParentBuilder WithFirstName(string first)
+        {
+            _parent.Name.First = first;
+            return this;
+        }
+
+        public ParentBuilder WithMiddleName(string middle)
+        {
+            _parent.Name.Middle = middle;
+            return this;
+        }
+
+        public ParentBuilder WithLastName(string last)
+        {
+            _parent.Name.Last = last;
+            return this;
+        }
+
+        public Parent Build()
+        {
+            return _parent;
+        }
+    }
+}
