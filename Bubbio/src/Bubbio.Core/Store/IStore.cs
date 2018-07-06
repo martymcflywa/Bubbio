7131783293e963806d02889d86ad85cfdcd912ad
Martin Ponce <martin.ponce@outlook.com>
2018-07-06 09:41:49 +0000

    Basic mongodb insert operations with BDD tests

---
+++ Bubbio/src/Bubbio.Core/Store/IStore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bubbio.Core.Store
+{
+    public interface IStore<TEntity, in TKey> : IQueryable<TEntity>
+        where TEntity : IEntity<TKey>
+    {
+        Task InsertAsync(TEntity entity);
+        Task InsertAsync(IEnumerable<TEntity> entities);
+
+        Task<IEnumerable<TEntity>> GetAsync(TKey id);
+        Task<IEnumerable<TEntity>> GetAsync(TEntity entity);
+
+        Task<long> CountAsync();
+        Task DeleteAllAsync();
+    }
+}
